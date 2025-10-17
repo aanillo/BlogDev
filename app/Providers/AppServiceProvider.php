697d92d9565d4fcfe721e1793b509e5284f52ee7
Alfonso <anillo.roalf23@cadiz.salesianos.edu>
2025-10-17 16:13:09 +0200

    cambios en AppService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
         View::composer('*', function ($view) {
         if (Auth::check()) {
             $view->with('notifications', Auth::user()->unreadNotifications);
